94624ffef341da6867a7a660cd972927f4107505
Christopher Natale <christophernatale@ChristophersAir.lan>
2020-11-24 19:00:40 +1100

    for deploy

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ let PORT = process.env.PORT || 3000;
 // Set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
-  });
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 require("./public/routes/apiRoutes")(app);
 require("./public/routes/htmlRoutes")(app);
